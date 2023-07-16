e7d857d5e2a0093376d73f90b76c9ab97509698a
devpetitprince <87466890+devpetitprince@users.noreply.github.com>
2023-07-16 15:52:09 +0900

    Add files via upload

---
+++ lottery-v2-api-server/models/Wallets.js
@@ -0,0 +1,55 @@
+const_ = require("lodash");
+const path = require("path");
+const envType = process.env.NODE_ENV || "development";
+require("dotenv").config({ path: path.join (_dirname, `../config/${envType}.env`)});
+
+
+module.exports = (sequelize, DataTypes) => {
+    const Wallet = sequelize.define("Wallet", {
+        id: {
+            type: DataTypes. INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        account_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        private_key: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        is_master: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: "created_at",
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: "updated_at"
+        },
+    }, {
+        freezeTableName: true,
+    });
+
+    Wallet.sync().then(async () => {
+        const masterWallet = await Wallet.findOne({ where: { account: process.env.MASTER_WALLET_ADDRESS }});
+        if (_.isEmpty(masterWallet)) {
+            await Wallet.create({
+                account_name: "master",
+                account: process.env.MASTER_WALLET_ADDRESS,
+                private_key: process.env.MASTER_WALLET_PRIVATE_KEY,
+                is_master: true,
+            });
+        }
+    });
+
+    return Wallet;
+};
